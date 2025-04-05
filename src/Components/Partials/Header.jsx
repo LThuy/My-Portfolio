a58b81b82e331b22f6866fcc957dc35417f129ca
LThuy <huytraceraner@gmail.com>
2025-04-05 18:05:57 +0700

    fix header

--- src/Components/Partials/Header.jsx
+++ src/Components/Partials/Header.jsx
@@ -1,17 +1,44 @@
-import React, { useState, useRef, useEffect } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
-import '../../Style/PartialsCSS/Header.scss';
+import React, { useState, useEffect } from "react";
+import "../../Style/PartialsCSS/Header.scss";
 import TypingHeader from "../Parts/TypingHeader";
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-// import '../../Style/Responsive/Responsive.css'
-// import axios from 'axios';
+
 function Header() {
-    return ( 
-        <header className="modern-header">
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  const updateBackgroundImage = () => {
+    const header = document.querySelector('.modern-header'); 
+
+    if (header) {
+      if (window.innerWidth > 700) {
+        header.style.background = 'url("background.jpeg") center/cover no-repeat';
+      } else {
+        header.style.background = 'url("background-3.jpg") center/cover no-repeat';
+      }
+    }
+  };
+
+  useEffect(() => {
+    updateBackgroundImage();
+    window.addEventListener('resize', updateBackgroundImage);
+    return () => {
+      window.removeEventListener('resize', updateBackgroundImage);
+    };
+  }, []); 
+
+  return (
+    <header className="modern-header">
       <nav className="nav">
+        <div className="hamburger" onClick={toggleMenu}>
+          <div className={`bar ${menuOpen ? "open" : ""}`}></div>
+        </div>
         <div className="logo">HUY DEV</div>
-        <ul className="nav-menu">
+        <ul className={`nav-menu ${menuOpen ? "show" : ""}`}>
           <li><a href="#hero">About</a></li>
+          <li><a href="#skills">Skills</a></li>
           <li><a href="#projects">Projects</a></li>
           <li><a href="#contact">Contact</a></li>
         </ul>
@@ -21,12 +48,12 @@ function Header() {
         <div className="profile-container">
           <img src="/profile.jpeg" alt="Your Name" className="profile-img" />
         </div>
-        {<TypingHeader/>}
+        <TypingHeader />
         <p>Web Developer | Designer | Innovator</p>
         <a href="#projects" className="cta-button">View My Work</a>
       </div>
     </header>
-    ); 
+  );
 }
 
-export default Header
+export default Header;
